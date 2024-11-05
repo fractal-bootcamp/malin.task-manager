90d93dedbe9c15db6ac938d0991be61157308ed8
mahlen <malink027@gmail.com>
2024-11-04 19:01:27 -0500

    updating global state store

--- task-manager/src/app/store/TaskStore.ts
+++ task-manager/src/app/store/TaskStore.ts
@@ -1,16 +1,19 @@
 import { create } from 'zustand'
+import type { TaskProps, TaskStatus } from '@/types/types'
 
 type TaskState = {
-  tite: string;
-  description: string;
-  state: 'pending' | 'in-progress' | 'completed' | 'archive';
+  tasks: TaskProps[]
+  createTask: (task: TaskProps) => void;
+  deleteTask: (taskId: string) => void;
+  updateTask: (taskId: string) => void;
+  updateTaskStatus: (taskId: string, newTaskStatus: TaskStatus) => void;
 }
 
-export const PendingStore = create((set) => ({
-  
-}))
-
-
 export const TaskStore = create<TaskState>((set) => ({
-  
+  tasks: [],  // Initialize empty array
+  createTask: (task: TaskProps) => 
+    set((state) => ({ 
+      tasks: [...state.tasks, task] 
+    })),
+
 }))
