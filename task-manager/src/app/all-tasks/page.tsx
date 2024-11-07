aa6c9cc19faa48da3e5c0f68e5a9fc66ea329861
mahlen <malink027@gmail.com>
2024-11-06 21:22:01 -0500

    all tasks page and changed the columns to not take up all the vertical space

---
+++ task-manager/src/app/all-tasks/page.tsx
@@ -0,0 +1,12 @@
+import { AllTasks } from "@/components/ui/TaskList";
+
+const Page = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">All Tasks</h1>
+      <AllTasks />
+    </div>
+  );
+};
+
+export default Page;
